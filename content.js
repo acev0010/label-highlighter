d5bb1110b79d3bf0e53754f5733a63468c117334
DhruvPatel2502 <93792385+DhruvPatel2502@users.noreply.github.com>
2022-12-05 10:18:26 -0500

    added functionality to the button

--- content.js
+++ content.js
@@ -1,7 +1,24 @@
 
-
 const labels = document.getElementsByTagName("label");
 
 for(label of labels) {
     label.style.border = "4px solid red";
  }
+function high(){
+    labels = document.getElementsByTagName('label');
+    for( var i = 0; i < labels.length; i++ ) {
+       if (labels[i].getAttribute("for")){
+       labels[i].style.border = "4px solid red";}
+       
+    }}
+    // javascript: (() => {
+    //     labels = document.getElementsByTagName('label');
+    // for( var i = 0; i < labels.length; i++ ) {
+    //    if (labels[i].getAttribute("for")){
+    //    labels[i].style.border = "4px solid red";}
+       
+    //   }})();
+
+// document.getElementById("btn2").addEventListener("click", high);
+
+
